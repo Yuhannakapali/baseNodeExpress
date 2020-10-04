cbe2f35b9b03c4ad6ff6e66f8f194ad9d4a00a75
yuhanna <yuhanna@kodiary.com>
2020-10-04 14:00:30 +0545

    file formated

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-const express = require('express')
-const app = express()
-const cookieParser = require('cookie-parser')
-const hbs = require('express-handlebars');
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const hbs = require("express-handlebars");
 
-const AppMiddleware = require('./app/middlewares/app.middleware')
+const AppMiddleware = require("./app/middlewares/app.middleware");
 
-app.use(cookieParser())
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(express.static('public/assets'))
-app.use(AppMiddleware.AppMiddleware)
-app.engine('hbs', hbs({
-    extname: 'hbs',
-    defaultLayout: 'layouts/app',
-    layoutsDir: __dirname + '/public/views/',
-    partialsDir: __dirname + '/public/views/layouts/partials'
-}))
-app.set('views', 'public/views')
-app.set('view engine', 'hbs')
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.static("public/assets"));
+app.use(AppMiddleware.AppMiddleware);
+app.engine(
+  "hbs",
+  hbs({
+    extname: "hbs",
+    defaultLayout: "layouts/app",
+    layoutsDir: __dirname + "/public/views/",
+    partialsDir: __dirname + "/public/views/layouts/partials",
+  })
+);
+app.set("views", "public/views");
+app.set("view engine", "hbs");
 
-require('./routes')(app)
+require("./routes")(app);
 
-app.listen(3000, _ => {
-    console.log("The server is running on Port 3000")
-})
+app.listen(3000, (_) => {
+  console.log("The server is running on Port 3000");
+});
